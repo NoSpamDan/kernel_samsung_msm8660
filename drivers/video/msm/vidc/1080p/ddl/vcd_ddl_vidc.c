318fd1aee02ba46c17ae782efbe9fecf0ab629c4
Dan Cartier <nospamdan1@gmail.com>
2016-01-11 03:13:17 -0500

    drivers: Fix merge error

--- drivers/video/msm/vidc/1080p/ddl/vcd_ddl_vidc.c
+++ drivers/video/msm/vidc/1080p/ddl/vcd_ddl_vidc.c
@@ -627,17 +627,8 @@ void ddl_vidc_encode_init_codec(struct ddl_client_context *ddl)
 		[ddl->command_channel], hdr_ext_control,
 		r_cframe_skip, false, 0,
 		h263_cpfc_enable, encoder->sps_pps.sps_pps_for_idr_enable_flag,
-<<<<<<< HEAD
-/* MMRND_AVRC. Start */
-#if 1
-=======
->>>>>>> 1a5d7a6... msm: vidc: Add support for Picture Order Count Type
 		encoder->pic_order_cnt_type, encoder->closed_gop, encoder->
 		avc_delimiter_enable, encoder->vui_timinginfo_enable,
-#else
-		encoder->closed_gop, encoder->avc_delimiter_enable,
-		encoder->vui_timinginfo_enable,
-#endif
 		encoder->bitstream_restrict_enable, ltr_enable);
 		
 	if (encoder->vui_timinginfo_enable) {
